6a11eef449b96f377e8e8cdaa3560ff41a1812ee
Zayat <zayat.ace@gmil.com>
2020-05-17 15:47:19 +0200

    Dar-elteb-1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <Header/>
+      <Doc/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from '@/components/Header'
+import Doc from '@/components/Doc'
+import $ from 'jquery'
+import niceScroll from 'jquery.nicescroll'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    Header,
+    Doc
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  mounted() {
+    $('html').niceScroll({
+      cursorborder: 'none'
+    })
+
+    AOS.init({
+      duration: 1000,
+      once: true
+    })
   }
-}
+};
+</script>
+
+<style lang="scss">
+@import './assets/config';
 </style>
---
+++ src/components/Doc.vue
@@ -0,0 +1,51 @@
+<template>
+  <main class="Doc">
+    <v-row>
+      <v-col md="6" class="text" data-aos="fade-up">
+        <h2>مستشفي دار الطب بالمنصورة</h2>
+        <h4>"زود فرص ارباحك باستثمارك و عملك معنا"</h4>
+        <div class="box">ضمن نخبة من اكبر المستثمرين و الأطباء بالدقهلية</div>
+      </v-col>
+      <v-col md="6" data-aos="fade-up" data-aos-delay="500">
+        <img src="../assets/img/doc.png">
+      </v-col>
+    </v-row>
+  </main>
+</template>
+
+<style lang="scss">
+@import '../assets/config';
+
+.Doc {
+  padding: 50px;
+  color: $prim;
+
+  .text {
+    margin-top: 100px;
+    padding-right: 100px;
+    text-align: center;
+
+    h2 {
+      font-size: 3em;
+    }
+
+    h4 {
+      font-size: 1.5em;
+      margin-top: 20px;
+    }
+
+    .box {
+      border: 2px solid $prim;
+      border-radius: 5px;
+      font-size: 2.4em;
+      font-weight: 600;
+      max-width: 400px;
+      margin: auto;
+      padding: 20px;
+      margin-top: 150px;
+      cursor: pointer;
+      user-select: none
+    }
+  }
+}
+</style>
